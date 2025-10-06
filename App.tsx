e4e9397d9567ae59f83e71273c01cf5b712e0664
Ana Olmos <anaolmosmartin@gmail.com>
2025-10-06 16:16:08 +0200

    inicio proyecto

--- App.tsx
+++ App.tsx
@@ -2,10 +2,21 @@ import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
 export default function App() {
+
+  const COLOR_FONDO = "#121212"
+  const COLOR_TITULO = "FFDD99"
+  const COLOR_TEXTO = "ffffff" 
+
+
   return (
-    <View>
+    <View style={[styles.contenedorPrincipal,{backgroundColor:COLOR_FONDO}]}>
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  contenedorPrincipal: {
+    flex: 1,
+    flexDirection: "column"
+  }
+})
